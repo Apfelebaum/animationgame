28dd472f2f0d8a762be4023629c44c4912c2af7b
Paul <Paul@Paul-PC.Speedport_W723_V_Typ_A_1_01_011>
2015-10-27 13:42:57 +0100

    a

--- animationgame/src/animationgame/PaintableStar.java
+++ animationgame/src/animationgame/PaintableStar.java
@@ -1,5 +1,10 @@
 package animationgame;
 
-public class PaintableStar {
-
+public class PaintableStar  extends Rectangular implements Paintable{
+	public PaintableStar (double width, double hight, double x, double y){
+		super (width, hight, x, y);
+	}
+	public void paintTo(java.awt.Graphics g){
+		
+	}
 }
